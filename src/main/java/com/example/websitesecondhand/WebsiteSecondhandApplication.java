041225d90b47cf231162cb86685a4697b139b81e
Viktor <3952324@gmail.com>
2023-06-28 18:57:20 +0200

    init commit

---
+++ src/main/java/com/example/websitesecondhand/WebsiteSecondhandApplication.java
@@ -0,0 +1,13 @@
+package com.example.websitesecondhand;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebsiteSecondhandApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebsiteSecondhandApplication.class, args);
+    }
+
+}
